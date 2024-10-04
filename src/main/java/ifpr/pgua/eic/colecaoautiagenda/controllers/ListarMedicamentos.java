0a7e36fb6ce043cca893c3c379cbbcacedc00499
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-10-04 15:27:06 -0300

    Finalizando o design das telas de listagem e de atualizar agendamentos

--- src/main/java/ifpr/pgua/eic/colecaoautiagenda/controllers/ListarMedicamentos.java
+++ src/main/java/ifpr/pgua/eic/colecaoautiagenda/controllers/ListarMedicamentos.java
@@ -1,5 +1,32 @@
 package ifpr.pgua.eic.colecaoautiagenda.controllers;
 
+import ifpr.pgua.eic.colecaoautiagenda.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class ListarMedicamentos {
-    
+   
+    @FXML
+    private ListView<?> listaDeAgendamentos;
+
+    @FXML
+    void concluirAgendamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletarAgendamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarAgendamento(ActionEvent event) {
+        App.pushScreen("ATUALIZARMEDICAMENTO");
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+        App.pushScreen("MENUPRINCIPALLISTAR");
+    }
 }
--- src/main/java/ifpr/pgua/eic/colecaoautiagenda/controllers/ListarTerapias.java
+++ src/main/java/ifpr/pgua/eic/colecaoautiagenda/controllers/ListarTerapias.java
@@ -1,5 +1,33 @@
 package ifpr.pgua.eic.colecaoautiagenda.controllers;
 
+import ifpr.pgua.eic.colecaoautiagenda.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class ListarTerapias {
-    
+   
+    @FXML
+    private ListView<?> listaDeAgendamentos;
+
+    @FXML
+    void concluirAgendamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletarAgendamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarAgendamento(ActionEvent event) {
+        App.pushScreen("ATUALIZARTERAPIA");
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+        App.pushScreen("MENUPRINCIPALLISTAR");
+    }
+
 }
